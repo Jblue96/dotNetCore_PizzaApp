6b64d8f920a00009b00ce4a6ab457ef3c3612ab8
Jorge Prime <jorgeprime@386.local>
2017-07-26 22:10:35 -0500

    Pizza Class build-out complete

--- Pizza/Models/Pizza.cs
+++ Pizza/Models/Pizza.cs
@@ -3,8 +3,16 @@ namespace Pizza.Models
 {
     public class Pizza
     {
-        public Pizza()
-        {
-        }
-    }
+        public int PizzaId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string AllergyInformation { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsPizzaOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+        public virtual Category Category { get; set; } 
+
 }
