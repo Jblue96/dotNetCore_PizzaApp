e62b001ef95a429c6fbcae756c6704facbc51757
Jorge Prime <jorgeprime@386.local>
2017-07-27 19:05:42 -0500

    mock repos added

---
+++ Pizza/Models/MockCategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Pizza.Models
+{
+    public class MockCategoryRepository
+    {
+        public MockCategoryRepository; ICategoryrepository
+        {
+            public IEnumerble<Category> Categories
+            {
+                get
+                {
+                return new List<Category>
+                    {
+                        new Category{CategoryId=1, CategoryName="Popular Pizzas", Description="All-the-time" }
+                        new Category{CategoryId=2, CategoryName="Distinct Pizzas", Description="Unique Flavor" }
+                        new Category{CategoryId=3, CategoryName="Seasonal Pizzas", Description="Octoberfest" }
+
+                    };
+                }
+                    
+            }
+
+        }
+    }
+}
---
+++ Pizza/Models/MockPieRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizza.Models
+{
+    public class MockPizzaRepository : IPizzaRepository
+    {
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+
+        public IEnumerable<Pizza> Pizzas
+        {
+            get
+            {
+                return new List<Pizza>
+                    {
+                        new Pizza(){pizzaId = 1, Name="Strawberry Pizza", Price=15.95M, ShortDescription="lorem ipsum"}
+                        new Pizza(){pizzaId = 2, Name="Cheese Pizza", Price=12.95M, ShortDescription="lorem ipsum"}
+
+                        new Pizza(){pizzaId = 3, Name="Rhubarb Pizza", Price=15.95M, ShortDescription="lorem ipsum"}
+                        new Pizza(){pizzaId = 4, Name="Pumpkin Pizza", Price=18.95M, ShortDescription="lorem ipsum"}
+                };
+                }
+        }
+        public IEnumerable<Pizza> PizzasOfTheWeek { get; }
+        public Pizza GetPizzaById(int pizzaID)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
